3849fbce9092eeaf3be7d00477a9b17a173fa9e1
Leonardo Cardoso <leonardo.t.cardoso@hotmail.com>
2016-09-23 13:05:03 -0300

    complete assigment

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 (function() {
   'use strict'
 
-  angular.module('myFirstAngularApp',[])
-
-  .controller('myFirstAngularController',myFirstAngularController)
-
   var myFirstAngularController = function($scope) {
-    
+    //start as an empty list
+    $scope.lunchList = '';
+    $scope.tooMuchMsg = 'Enjoy!';
+
+    $scope.isTooMuch = function() {
+      var str = $scope.lunchList;
+      var checkEmpt
+
+      if(str.length > 0) {
+
+        $scope.tooMuchMsg = (str.includes(",")
+          && str.split(',').filter(function(el) { return el.length > 0 }).length > 3)? 
+          'Too much!' : 'Enjoy!';
+
+      }
+      else {
+        $scope.tooMuchMsg = 'Please enter data first';
+      }
+    };
   };
 
   myFirstAngularController.$inject = ['$scope'];
+
+
+  angular.module('myFirstAngularApp',[])
+    .controller('myFirstAngularController',myFirstAngularController);
 
 })();
